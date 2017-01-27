c1c46f5a4a184b2f48f596e45f8a644dfc1582b8
cdr135 <cdr300947@gmail.com>
2017-01-26 22:28:36 -0800

    Moved Statics utility functions to Utils

--- src/testplayer/res/Utils.java
+++ src/testplayer/res/Utils.java
@@ -26,4 +26,59 @@ public abstract class Utils {
 	public static Direction direction(MapLocation ml1, MapLocation ml2) {
 		return ml1.directionTo(ml2);
 	}
+	
+	public static MapLocation[] combineLocs(MapLocation[]... arrs) {
+		int length = 0;
+		for (MapLocation[] arr : arrs)
+			length += arr.length;
+		int i = 0;
+		MapLocation[] all = new MapLocation[length];
+		for (MapLocation[] arr : arrs)
+			for (MapLocation loc : arr)
+				all[i++] = loc;
+
+		return all;
+	}
+
+	public static RobotInfo[] combineRobotInfo(RobotInfo[]... arrs) {
+		int length = 0;
+		for (RobotInfo[] arr : arrs)
+			length += arr.length;
+		int i = 0;
+		RobotInfo[] all = new RobotInfo[length];
+		for (RobotInfo[] arr : arrs)
+			for (RobotInfo loc : arr)
+				all[i++] = loc;
+
+		return all;
+	}
+
+	@Deprecated
+	public static Object[] combineArrays(Object[]... arrs) {
+		int length = 0;
+		for (Object[] arr : arrs) {
+			length += arr.length;
+		}
+		System.out.println("length: = " + length);
+		Object[] hi = new Object[length];
+		int i = 0;
+		for (Object[] arr : arrs) {
+			for (Object o : arr) {
+				hi[i++] = o;
+			}
+		}
+		System.out.println("final i: =" + i);
+		if (length == 0 && i == 0) {
+			hi = new MapLocation[] { new MapLocation(0, 0) };
+			System.out.println("test: " + hi);
+		}
+		return hi;
+	}
+
+	public static boolean contains(Object obj, Object[] arr) {
+		for (Object i : arr)
+			if (i.equals(obj))
+				return true;
+		return false;
+	}
 }
