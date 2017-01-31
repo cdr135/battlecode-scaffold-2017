0c9b8d2de5a02c8a04b4edfa355a17e730184e16
cdr135 <cdr300947@gmail.com>
2017-01-30 16:27:59 -0800

    Revert "Revert "Globals other""
    This reverts commit 2f263d4d768edd8b429978bee60c4895dabf6dcd.

--- src/testplayer/res/Globals.java
+++ src/testplayer/res/Globals.java
@@ -3,7 +3,11 @@ package testplayer.res;
 public abstract class Globals {
 	// constants used in this context
 	
+	//arbitrary
+	public static final int MAX_SCOUTS = 9;
+	
 	
 	
 	// globally used variables
+	public static int numScouts = 0;
 }
